130e369cf8032be42a15e143981ee21bd331b2ef
itlbv <artem.v.fokin@gmail.com>
2019-05-31 12:00:57 +0200

    Add model layer with blank classes AbstractEntity and Routine

---
+++ src/main/java/com/itlbv/routineplanner/model/AbstractEntity.java
@@ -0,0 +1,27 @@
+package com.itlbv.routineplanner.model;
+
+public abstract class AbstractEntity {
+    private int id;
+    private String name;
+
+    AbstractEntity(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/itlbv/routineplanner/model/Routine.java
@@ -0,0 +1,7 @@
+package com.itlbv.routineplanner.model;
+
+public class Routine extends AbstractEntity {
+    public Routine(int id, String name) {
+        super(id, name);
+    }
+}
