f744f1e3891bb4e86568bf4068e9b1c6d98bf523
itlbv <artem.v.fokin@gmail.com>
2019-07-25 17:00:25 +0200

    Add initialization and population of users table into SQL scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,10 +1,17 @@
 -- noinspection SqlWithoutWhere
-
+DELETE
+FROM users;
+-- noinspection SqlWithoutWhere
 DELETE
 FROM routines;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
+INSERT INTO users(name, email)
+VALUES ('user1', 'email1'),
+       ('user2', 'email2'),
+       ('user3', 'email3');
+
 INSERT INTO routines(name)
-VALUES ('name1'),
-       ('name2'),
-       ('name3');
+VALUES ('routine1'),
+       ('routine2'),
+       ('routine3');
