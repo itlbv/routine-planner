1ae7d0c342eeb60c76744f453e2814cbdbbb76c3
itlbv <artem.v.fokin@gmail.com>
2019-08-01 16:52:47 +0200

    Make RoutineServiceImplTest work with userId parameter

--- src/test/java/com/itlbv/routineplanner/service/RoutineServiceImplTest.java
+++ src/test/java/com/itlbv/routineplanner/service/RoutineServiceImplTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.itlbv.routineplanner.TestData.ROUTINES;
+import static com.itlbv.routineplanner.TestData.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoutineServiceImplTest extends AbstractServiceTest {
@@ -18,48 +18,55 @@ class RoutineServiceImplTest extends AbstractServiceTest {
 
     @Test
     void create() {
-        Routine routineNew = new Routine(null, "routineNew");
-        Routine routineDB = service.create(routineNew);
+        Routine routineNew = new Routine(null, USER_1, "routineNew");
+        Routine routineDB = service.create(routineNew, USER_1.getId());
         routineNew.setId(routineDB.getId());
-        List<Routine> routinesExpected = new ArrayList<>(ROUTINES);
+        List<Routine> routinesExpected = new ArrayList<>(USER_1_ROUTINES);
         routinesExpected.add(routineNew);
-        assertArrayEquals(routinesExpected.toArray(), service.getAll().toArray());
+        assertArrayEquals(routinesExpected.toArray(), service.getAll(USER_1.getId()).toArray());
     }
 
     @Test
     void update() {
-        Routine routineExpected = new Routine(100003, "testUpdate");
-        Routine routineDB = service.get(100003);
+        Routine routineExpected = new Routine(100003, USER_1, "testUpdate");
+        Routine routineDB = service.get(100003, USER_1.getId());
         routineDB.setName("testUpdate");
-        service.update(routineDB);
-        assertEquals(routineExpected, service.get(100003));
+        service.update(routineDB, USER_1.getId());
+        assertEquals(routineExpected, service.get(100003, USER_1.getId()));
+    }
+
+    @Test
+    void updateNotFound() {
+        Routine routine = service.get(100003, USER_1.getId());
+        routine.setName("testUpdate");
+        assertThrows(NotFoundException.class, () -> service.update(routine, 0));
     }
 
     @Test
     void delete() {
-        List<Routine> routinesExpected = new ArrayList<>(ROUTINES);
+        List<Routine> routinesExpected = new ArrayList<>(USER_1_ROUTINES);
         routinesExpected.remove(0);
-        service.delete(100003);
-        assertArrayEquals(routinesExpected.toArray(), service.getAll().toArray());
+        service.delete(100003, USER_1.getId());
+        assertArrayEquals(routinesExpected.toArray(), service.getAll(USER_1.getId()).toArray());
     }
 
     @Test
     void deleteNotFound() {
-        assertThrows(NotFoundException.class, () -> service.delete(0));
+        assertThrows(NotFoundException.class, () -> service.delete(0, USER_1.getId()));
     }
 
     @Test
     void get() {
-        assertEquals(ROUTINES.get(0), service.get(100003));
+        assertEquals(ROUTINES.get(0), service.get(100003, USER_1.getId()));
     }
 
     @Test
     void getNotFound() {
-        assertThrows(NotFoundException.class, () -> service.get(0));
+        assertThrows(NotFoundException.class, () -> service.get(0, USER_1.getId()));
     }
 
     @Test
     void getAll() {
-        assertArrayEquals(ROUTINES.toArray(), service.getAll().toArray());
+        assertArrayEquals(USER_1_ROUTINES.toArray(), service.getAll(USER_1.getId()).toArray());
     }
 }
