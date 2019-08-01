fd3e6304df15c36966d5c00a2773bd761ccd41c0
itlbv <artem.v.fokin@gmail.com>
2019-08-01 15:57:47 +0200

    Add field user with getter and setter to Routine class

--- src/main/java/com/itlbv/routineplanner/model/Routine.java
+++ src/main/java/com/itlbv/routineplanner/model/Routine.java
@@ -2,10 +2,21 @@ package com.itlbv.routineplanner.model;
 
 public class Routine extends AbstractEntity {
 
+    private User user;
+
     public Routine() {
     }
 
-    public Routine(Integer id, String name) {
+    public Routine(Integer id, User user, String name) {
         super(id, name);
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
